c865d545d721b012a9e8708e6c974288ddb561bb
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-10-06 00:46:27 +0400

    some upgrades

--- src/context/wagmi.ts
+++ src/context/wagmi.ts
@@ -1,18 +1,13 @@
-import { customWalletConnector } from '@/lib/customConnector';
-import { createConfig, http } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
-import { injected, metaMask, safe } from 'wagmi/connectors'
+import { customWalletConnector } from "@/lib/customConnector";
+import { createConfig, http } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { injected, metaMask, safe } from "wagmi/connectors";
+import { createClient } from "viem";
 
 export const config = createConfig({
-  connectors: [customWalletConnector, injected(),
-    metaMask(),
-    safe(),], // Pass the function itself, not the result of calling it
+  connectors: [customWalletConnector, injected(), metaMask(), safe()], // Pass the function itself, not the result of calling it
   chains: [base, polygon, optimism, arbitrum, mainnet],
-  transports: {
-    [base.id]: http(),
-    [mainnet.id]: http(),
-    [polygon.id]: http(),
-    [optimism.id]: http(),
-    [arbitrum.id]: http(),
+  client({ chain }) {
+    return createClient({ chain, transport: http() });
   },
 });
